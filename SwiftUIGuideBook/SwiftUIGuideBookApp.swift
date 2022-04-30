1094c98ba52c0e3067d04ac4f90a93b37f2da867
MMMIIIN <wbsldj7645@naver.com>
2022-04-30 17:56:04 +0900

    Initial Commit

---
+++ SwiftUIGuideBook/SwiftUIGuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGuideBookApp.swift
+//  SwiftUIGuideBook
+//
+//  Created by Mingwan Choi on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
