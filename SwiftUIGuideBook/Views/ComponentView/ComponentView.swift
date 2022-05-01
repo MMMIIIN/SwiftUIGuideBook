1aeeb71cbe7cbad4396585657c0d5475c6da4ed9
MMMIIIN <wbsldj7645@naver.com>
2022-05-01 23:40:55 +0900

    Feat: TipList

---
+++ SwiftUIGuideBook/Views/ComponentView/ComponentView.swift
@@ -0,0 +1,20 @@
+//
+//  ComponentView.swift
+//  SwiftUIGuideBook
+//
+//  Created by Mingwan Choi on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ComponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComponentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComponentView()
+    }
+}
